f59dd0081d648502a56cea75eb5c29122fd28175
LIN-25\Lin <424472969@qq.com>
2017-02-08 14:12:39 +0800

    1

--- src/t1/T.java
+++ src/t1/T.java
@@ -2,10 +2,10 @@ package t1;
 
 public class T {
 	public static void main(String[] args) {
-		System.out.println("主干1");
+		System.out.println("主干4");
 		System.out.println("主干3");
 		System.out.println("主干2");
-		
+		System.out.println("主干1");
 		
 	}
 }
