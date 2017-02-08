3c79cb47e9b2e8f5c038917fcad183c89be6b573
424472969 <424472969@qq.com>
2017-02-08 14:21:34 +0800

    Update T.java

--- src/t1/T.java
+++ src/t1/T.java
@@ -2,14 +2,7 @@ package t1;
 
 public class T {
 	public static void main(String[] args) {
-		System.out.println("主干6");
 		System.out.println("主干5");
-		System.out.println("主干3");
-		System.out.println("主干2");
-		System.out.println("主干1");
-		System.out.println("主干0");
-		
-				
 		
 	}
 }
